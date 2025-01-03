331b73154718df53de137f5901a5c588d17391c4
vinay-kumar-3 <129206970+vinay-kumar-3@users.noreply.github.com>
2025-01-03 23:35:55 +0530

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,7 +5,7 @@ export default function Stats() {
   const [statInfo, setStatInfo] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/stat-info")
+    fetch("https://battlefield4-backend-7blnib22r-vinay-kumars-projects-c35f172e.vercel.app/api/stat-info")
       .then((response) => response.json())
       .then((data) => setStatInfo(data.data)); // Assuming `data` contains the "data" array
   }, []);
