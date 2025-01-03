51535ee7d23b64f1a872902a2168b42ff699b3c7
vinay-kumar-3 <129206970+vinay-kumar-3@users.noreply.github.com>
2025-01-03 23:35:28 +0530

    Update ServerInfo.jsx

--- src/Pages/ServerInfo.jsx
+++ src/Pages/ServerInfo.jsx
@@ -6,7 +6,7 @@ export default function ServerInfo() {
   const [serverInfo, setServerInfo] = useState({ settings: [], advanced: [], rules: [] });
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/server-info")
+    fetch("https://battlefield4-backend-7blnib22r-vinay-kumars-projects-c35f172e.vercel.app/api/server-info")
       .then((response) => response.json())
       .then((data) => setServerInfo(data));
   }, []);
